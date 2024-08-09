c07c447a4e37ef16aeb67a49da019a9883d8ddcb
Sanket Kachare <sanketsk74@gmail.com>
2024-08-09 14:38:45 +0530

    Added functionality for play again. created function for initializing the values or restore to initial values and applied the same

--- script.js
+++ script.js
@@ -20,17 +20,40 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+// let scores;
+// let currentScore;
+// let activePlayer;
+// let playing;
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// or we can declare in one line like below
+let scores,currentScore,activePlayer,playing;
+// Starting conditions or restoring the all values to initial stage
+
+const init = function(){
+
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    diceEl.classList.add("hidden");
 
 
-const scores = [0,0]
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current0El.textContent = 0;
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+
+//  calling init() function
+init();
 
 //switch to next player function
 const switchPlayer = function(){
@@ -104,11 +127,12 @@ btnHold.addEventListener('click',function(){
     //2. Check if player's score is >=100
     // finish the game
     if(scores[activePlayer]>=20){
+        playing = false;
 
         diceEl.classList.add('hidden');
 
-        console.log(`Active palyer : ${activePlayer}`)
-        playing = false;
+        // console.log(`Active palyer : ${activePlayer}`)
+        
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
 
         document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
@@ -118,5 +142,8 @@ btnHold.addEventListener('click',function(){
         switchPlayer();
     }
 }
-})
+});
+
+// new game functionality
+btnNew.addEventListener('click',init);
  
