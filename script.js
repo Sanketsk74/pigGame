014b23650406410c25a80c37c3b8c13968956e52
Sanket Kachare <sanketsk74@gmail.com>
2024-08-09 12:05:10 +0530

    This is first commit for pigGame

---
+++ script.js
@@ -0,0 +1,102 @@
+'use strict';
+
+
+// selecting elements
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+const current0El = document.querySelector('#current--0');
+const current1El = document.getElementById('current--1');
+
+
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+
+const scores = [0,0]
+let currentScore = 0;
+let activePlayer = 0;
+
+//switch to next player function
+const switchPlayer = function(){
+            // switch to next player
+
+
+        // setting the current score value of 1st player to 0.
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+        currentScore=0;
+
+        // change the value pf active player to switch player
+        // we will change this value by using turnary operator
+        activePlayer = activePlayer=== 0 ? 1 : 0 ;
+
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+
+}
+
+//Rolling the dice functionality
+
+btnRoll.addEventListener('click',function(){
+
+    // 1. Generating a random number between 1 to 6 for dice roll
+
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+
+
+    //2. Display dice
+
+    diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove('hidden');
+    console.log(dice);
+
+    //3. Check for rolled dice number is 1 or not : if true, then switch to next player
+    if(dice !== 1){
+        // Add dice number to current score
+        currentScore+=dice;
+
+        // // manually selecting current score element
+        // currentScore0El.textContent = currentScore; // CHANGE LATER
+
+        // Selecting current score element dynamically
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
+    }else{
+        // switch to next player
+        switchPlayer();
+    }
+
+
+});
+
+btnHold.addEventListener('click',function(){
+
+    //1. Add Current score to active playes's score
+    scores[activePlayer]+=currentScore;
+    // scores[1] = scores[1] +  currentScore
+
+    document.getElementById(`current--${activePlayer}`).textContent = scores[activePlayer];
+
+    //2. Check if player's score is >=100
+    // finish the game
+
+    //Switch to the next player
+    switchPlayer();
+})
+ 
